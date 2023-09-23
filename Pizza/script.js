7a7893bf9fcc2672a7cd19fb8ce5c86c4ae67538
Laiba Ahmad <laib.khan.an@gmail.com>
2023-09-23 13:05:31 +0500

    Homa Made Pizza Website

---
+++ Pizza/script.js
@@ -0,0 +1,15 @@
+alert("Asslam-o-Alikum")
+
+alert("Please Enter this input we will never send your imformation to another user")
+prompt("What is your Name")
+prompt("What is your Email")
+var gender2 = prompt("Enter Your Gender")
+if(gender2 === "Male"){
+    alert("Hello, Sir")
+}
+else if(gender2 === "Female"){
+    alert("Hello, Sister")
+}
+else{
+    alert("Please Enter Your Gender")
+}
