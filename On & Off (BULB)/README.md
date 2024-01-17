7879cc37c5fe82c43961eb5b8d29748a94354653
Laiba Ahmad <134959431+Laiba-Ahmad@users.noreply.github.com>
2024-01-17 15:49:48 +0500

    ON/OFF BULB

---
+++ On & Off (BULB)/README.md
@@ -0,0 +1,2 @@
+# ON OFF BULB
+## [https://on-off-bulb.netlify.app](https://on-off-bulb.netlify.app)
---
+++ On & Off (BULB)/app.js
@@ -0,0 +1,8 @@
+function on(){
+    var a = document.getElementById('offbulb1')
+    a.src = 'https://bulbon-off-na.netlify.app/images/bulbon.webp'
+}
+function off(){
+    var a = document.getElementById('offbulb1')
+    a.src = 'https://bulbon-off-na.netlify.app/images/bulbof.webp'
+}
